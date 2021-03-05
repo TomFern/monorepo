5c41d63a42f190d11fb4edcb4a52d3b221e44e64
tomas f <46322567+TomFern@users.noreply.github.com>
2021-03-05 13:20:04 -0300

    add test routes

--- react-native/movies/App.js
+++ react-native/movies/App.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import { ActivityIndicator, FlatList, Text, View } from 'react-native';
 
-const baseUrl = 'https://reactnative.dev';
+const endpoint = 'https://reactnative.dev';
+// const endpoint = 'http://192.168.0.14:3000/movies';
+// for integration tests
+// const endpoint = 'http://localhost:3000/movies';
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch( baseUrl + '/movies.json')
+    fetch( endpoint )
       .then((response) => response.json())
       .then((json) => setData(json.movies))
       .catch((error) => console.error(error))
